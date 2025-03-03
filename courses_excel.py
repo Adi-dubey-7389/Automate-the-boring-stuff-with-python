1b5955da48913e78efa4a8e01ed9d986d0af5e8a
Adi-dubey-7389 <aadidubey7389@gmail.com>
2025-03-03 16:46:19 +0530

    Add files via upload

---
+++ courses_excel.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pa
+
+url = 'https://www.aksuniversity.ac.in/courses/All-Programs'
+
+web = requests.get(url)
+
+soup = BeautifulSoup(web.content,'html.parser')
+
+both_ = soup.find_all('div',class_ = 'card h-100')
+
+courses_data = []
+
+for both in both_:
+
+    name = both.find('h3').text
+
+    des = both.find('p').text
+
+    courses_data.append([name,des])
+
+for page_num in range(1, 5):
+
+    page_url = f'{url}?page={page_num}'
+
+    soup_ = BeautifulSoup(requests.get(page_url).content,'html.parser')
+
+    both__ = soup_.find_all('div',class_ = 'card h-100')
+
+    for bot in both__:
+
+        name = bot.find('h3').text
+
+        des = bot.find('p').text
+
+        courses_data.append([name,des])
+
+df = pa.DataFrame(courses_data)
+
+df.to_excel('courses.xlsx')
+
+        
+
+        
+    
+    
+
+
+
+
+
+
+
+
+
+
+
